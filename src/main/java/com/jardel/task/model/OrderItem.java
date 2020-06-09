3814bfb65e1ac1367cbe097bb5523bfb8a29cfb1
Jardel Kuhnen <jardel.kuhnen@hbsis.com.br>
2020-06-09 20:38:33 -0300

    Changing for postgresql

--- src/main/java/com/jardel/task/model/OrderItem.java
+++ src/main/java/com/jardel/task/model/OrderItem.java
@@ -17,7 +17,7 @@ import java.time.LocalDateTime;
 public class OrderItem {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     @Column
