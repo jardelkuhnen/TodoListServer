24cfde3c4418073b6ca9af731e391aff4fca3ec5
Jardel Kuhnen <jardel.kuhnen@hbsis.com.br>
2020-06-14 16:16:01 -0300

    Adding the module security

---
+++ security/src/test/java/com/tasks/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasks.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
