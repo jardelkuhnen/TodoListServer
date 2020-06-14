dd3a1830f51e20e90238dcf999a4ed6edc176cdb
Jardel Kuhnen <jardel.kuhnen@hbsis.com.br>
2020-06-14 18:52:51 -0300

    Adding security

---
+++ tasks-app/src/main/java/com/tasks/controller/auth/AuthenticationController.java
@@ -0,0 +1,27 @@
+package com.tasks.controller.auth;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+    private static final String TOKEN_HEADER = "Authorization";
+    private static final String BEARER_PREFIX = "Bearer ";
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+
+
+
+}
