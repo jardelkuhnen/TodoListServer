54499e5b35ec09343fbaf857bf35efcdcb5534ab
Jardel Kuhnen <jardel.kuhnen@hbsis.com.br>
2020-06-25 22:22:57 -0300

    test

--- tasks-app/src/main/java/com/tasks/TasksAppApplication.java
+++ tasks-app/src/main/java/com/tasks/TasksAppApplication.java
@@ -2,10 +2,11 @@ package com.tasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories({"com.tasks.domain.repository", "com.tasks.security.repository"})
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class TasksAppApplication {
 
     public static void main(String[] args) {
